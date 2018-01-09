dbff23e73ad90c7e00f66981a0dad09b70c46d6f
leonbec <dwfinz@gmail.com>
2018-01-09 19:58:56 +1300

    activity lifecycle intro

--- app/src/main/java/com/leonbec/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/leonbec/buttonclickapp/MainActivity.kt
@@ -3,25 +3,69 @@ package com.leonbec.buttonclickapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
-    private var times = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        userInput.setText("")
         textView.text = ""
         textView.movementMethod = ScrollingMovementMethod()
 
         button.setOnClickListener {
-            ++times
-            textView.append("The btn got tapped $times time")
-            if (times != 1)
-                textView.append("s")
+            Log.d(TAG, "onClick")
+            textView.append(userInput.text)
             textView.append("\n")
+            userInput.setText("")
         }
     }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        Log.d(TAG, "onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
 }
